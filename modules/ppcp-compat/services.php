b51b3bd67805b4f9274f1f96a84298be226f3272
Jorge A. Torres <jorge.torres@automattic.com>
2021-07-14 15:06:32 -0500

    Adds an importer for PPXO settings

--- modules/ppcp-compat/services.php
+++ modules/ppcp-compat/services.php
@@ -9,4 +9,12 @@ declare(strict_types=1);
 
 namespace WooCommerce\PayPalCommerce\Compat;
 
-return array();
+return array(
+
+	'compat.ppec.settings_importer' => static function( $container ) : PPEC\SettingsImporter {
+		$settings = $container->get( 'wcgateway.settings' );
+
+		return new PPEC\SettingsImporter( $settings );
+	},
+
+);
